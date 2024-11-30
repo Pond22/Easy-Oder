ae2de5d22d3ad0c1ddbfa8735818c79f575987d7
Pond22 <pondba22@gmail.com>
2024-11-30 15:38:19 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to show the video popup
+function showVideo() {
+    document.getElementById('video-popup').style.display = 'flex';
+  }
+
+  // Function to close the video popup
+  function closeVideo() {
+    document.getElementById('video-popup').style.display = 'none';
+  }
